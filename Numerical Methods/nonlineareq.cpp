018a462a7a1ce3e0d39629859f74ea779830ea7d
PWilkosz99 <pwilkosz99@gmail.com>
2021-05-31 23:07:41 +0200

    Regula Falsi

--- Numerical Methods/nonlineareq.cpp
+++ Numerical Methods/nonlineareq.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+#pragma region functions and derivatives
+
 double f1(double x) {
 	return pow(x, 3) + pow(x, 2) - (3 * x) - 3;
 }
@@ -34,20 +36,22 @@ double df4(double x) {
 	return 2 * x * (cos(pow(x, 2)) - 1);
 }
 
+#pragma endregion
+
 #define EPSILON 0.000001
 #define EPSILONTEST 0.1
 #define MAXITER 1000000
 
-
-bool CheckIfThisResultExist(double res, vector<double> ResultList) {
+bool CheckIfThisResultExist(double res, vector<double> ResultList)
+{
 	for (auto a : ResultList) {
 		if (a > res) {
-			if ((a-res)< EPSILONTEST) {
+			if ((a - res) < EPSILONTEST) {
 				return true;
 			}
 		}
 		else {
-			if ((res-a) < EPSILONTEST) {
+			if ((res - a) < EPSILONTEST) {
 				return true;
 			}
 		}
@@ -55,7 +59,8 @@ bool CheckIfThisResultExist(double res, vector<double> ResultList) {
 	return false;
 }
 
-void PrintZeros(vector<double> zrs) {
+void PrintZeros(vector<double> zrs)
+{
 	int i = 0;
 	for (auto n : zrs) {
 		cout << "x[" << i << "] = " << n << "\n";
@@ -119,23 +124,55 @@ double SecantMethod(func f, double x1, double x2)
 			{
 				break;
 			}
-			else 
+			else
 			{
 				xm = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1));
 			}
 		} while (fabs(xm - x0) >= EPSILON);
 		return x0;
 	}
-	else 
+	else
 	{
 		cout << "Brak rozwiazan dla przedzialu" << "[" << x1 << "]" << "\n";
 		throw "Brak rozwiazan";
 	}
 }
 
+double RegulaFalsi(func f, double x1, double x2)
+{
+	double x0;
+	if (f(x1) * f(x2) >= 0)
+	{
+		cout << "Funkcja przyjmuje te same znaki\n"; //Z zalozen reguly falsi wynika ze funkcja na krañcach przedzia³u [a,b] musi przyjmowac rozne znaki
+		throw "Zly przedzial dla reguly Falsi";
+	}
+	else 
+	{
+		for (int i = 0; i < MAXITER; i++)
+		{
+			x0 = (x1 * f(x2) - x2 * f(x1)) / (f(x2) - f(x1));
+
+			if (f(x0) == 0)
+			{
+				break;
+			}
+			else if (f(x0) * f(x1) < 0)
+			{
+				x2 = x0;
+			}
+			else
+			{
+				x1 = x0;
+			}
+		}
+	}
+	return x0;
+}
+
 void runable() {
 	//PrintZeros(NewtonRaphson(f1, df1, -10));
-	cout<<SecantMethod(f1, -100, 120);
+	//cout<<SecantMethod(f1, -100, 120);
+	cout<<RegulaFalsi(f1, -2, 5);
 }
 
 //1
