f45bd11ab4ba4e1b872cebf7fef9c4f6a0a2e374
PWilkosz99 <pwilkosz99@gmail.com>
2021-06-05 00:36:48 +0200

    Heun method

--- Numerical Methods/differentialeq.cpp
+++ Numerical Methods/differentialeq.cpp
@@ -1,5 +1,5 @@
 #include "headers.h"
-//#define SHOWSTEPS
+#define SHOWSTEPS
 
 using namespace std;
 
@@ -25,7 +25,24 @@ double EulerMethod(int n, double xs, double xf, double ys, funcTherm f, double a
 	return res;
 }
 
+double HeunMethod(int n, double xs, double xf, double ys, funcTherm f, double alpha, double beta)
+{
+	double res = ys;
+	double y0 = ys;
+	double h = (xf - xs) / n;
+	for (int i = 0; i < n; i++)
+	{
+		res += 0.5 * h * (f(alpha, beta, y0) + f(alpha, beta, y0 + h * f(alpha, beta, y0)));
+		y0 = res;
+#ifdef SHOWSTEPS
+		cout << "[" << i << "] " << res << "\n";
+#endif
+	}
+	return res;
+}
+
 void runable()
 {
-	cout << EulerMethod(1000, 1, 10, 300, ThermalConductivityEQ, -1E-8, 0);
+	cout << EulerMethod(10, 0, 300, 1200, ThermalConductivityEQ, -1E-12, 0);
+	cout << HeunMethod(10, 0, 300, 1200, ThermalConductivityEQ, -1E-12, 0);
 }
